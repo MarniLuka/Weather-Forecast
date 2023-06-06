ee8b267c6551c715ba9e832f93f8983bb23ef68e
Marni Luka <marnixluka@gmail.com>
2023-06-06 18:11:03 -0400

    main dashboard display logic. changed api key.

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,39 @@
+// API key to use Open Weather Map
+const APIKey = 'a698c9b2fcc8b543ace375d22e7dea94'
+// Targets the user input
+var city = document.getElementById("searchInput").value;
+// Targets the button
+var searchBtn = document.getElementById("searchBtn");
+// Empty array for previously searched cities
+var searchedCities = [];
+// Targets big dashboard dispaly
+var dashboardDisplay = document.getElementById('dashboardDisplay')
 
-const APIKey = 'cf1ba4c3d3d7d1cb78cbbf41fb56d86e'
+
+// function saveCity() {
+//     var cities = JSON.parse(localStorage.getItem(''))
+// }
+
+function weatherDisplay() {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey)
+        .then (function (response) {
+            if (response.status !== 200) {
+                alert('City Unavaialbe. Please Input a Valid City');
+                return;
+            }
+            return response.json();
+        })
+        .then (function (data) {
+            var date = dayjs().format('MM/DD/YYY');
+            $('#cardCity').textContent = data.name;
+            $('cardDate0').textContent = date;
+            $('cardTemp0').textContent = data.main.temp;
+            $('cardWind0').textContent = data.wind.speed;
+            $('cardHumidity0').textContent = data.main.humidity;
+
+            return
+        })
+}
+
+
+searchBtn.addEventListener('click', weatherDisplay);
